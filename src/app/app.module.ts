abb603b350ac48d750eb0dc71f2535a9b1039da6
sean <sdoody95@gmail.com>
2022-03-21 09:10:45 -0400

    created components, models, services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { SearchComponent } from './components/search/search.component';
+import { WatchListComponent } from './components/watch-list/watch-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MovieDetailComponent,
+    MovieListComponent,
+    SearchComponent,
+    WatchListComponent
   ],
   imports: [
     BrowserModule,
